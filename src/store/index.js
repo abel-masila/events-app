770368b0d1138f19f93ffba6831ba9a02a2e4138
Abel Masila <abelmasila@gmail.com>
2020-07-23 18:37:58 +0300

    used mapState to map computed variables

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,21 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      id: 1,
+      name: 'masila'
+    },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ]
+  },
   mutations: {},
   actions: {},
   modules: {}
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create An Event {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState(['user', 'categories'])
+}
+</script>
+
+<style></style>
