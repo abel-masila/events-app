33963a6c56c176d80c75d438ed0a39002152e84a
Abel Masila <abelmasila@gmail.com>
2020-07-23 18:45:41 +0300

    using getters and passing getters as args to getter

--- src/store/index.js
+++ src/store/index.js
@@ -17,9 +17,26 @@ export default new Vuex.Store({
       'education',
       'food',
       'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
     ]
   },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Create An Event {{ user.name }}</h1>
     <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
     <ul>
       <li v-for="cat in categories" :key="cat">{{ cat }}</li>
     </ul>
@@ -12,7 +13,12 @@
 import { mapState } from 'vuex'
 
 export default {
-  computed: mapState(['user', 'categories'])
+  computed: {
+    ...mapState(['user', 'categories']),
+    catLength() {
+      return this.$store.getters.catLength
+    }
+  }
 }
 </script>
 
