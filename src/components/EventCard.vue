b0902c89d90e1201c53c33f611b56207cf2090b5
Abel Masila <abelmasila@gmail.com>
2020-07-23 16:36:37 +0300

    worked on Navbar component and EventCard component

---
+++ src/components/EventCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <router-link
+    class="event-link"
+    :to="{ name: 'event-show', params: { id: 1 } }"
+  >
+    <div class="event-card -shadow">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <span>{{ event.attendies.length }} attending</span>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      event: {
+        id: 1,
+        title: 'Beach Cleanup',
+        date: 'Tues Aug 19, 2020',
+        time: '6:00',
+        attendies: [
+          {
+            id: 'abc',
+            name: 'Abel'
+          },
+          {
+            id: 'abc2',
+            name: 'Jaki'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
+</style>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <h1>Event List View</h1>
-    <router-link :to="{ name: 'event-show', params: { id: 1 } }"
-      >Show Event #1</router-link
-    >
+    <EventCard />
   </div>
 </template>
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
