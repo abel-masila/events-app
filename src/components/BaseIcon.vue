a23c8e983f1bd41ecd5ae6661ed550ed50870aca
Abel Masila <abelmasila@gmail.com>
2020-07-23 17:23:50 +0300

    added a slot for baseIcon

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="icon-wrapper" v-html="svg"></div>
+  <div class="icon-wrapper">
+    <svg class="icon" :width="width" :height="height">
+      <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }"></use>
+    </svg>
+    <slot></slot>
+  </div>
 </template>
-
 <script>
-import feather from 'feather-icons'
 export default {
+  name: 'Icon',
   props: {
     name: String,
     width: {
@@ -14,15 +18,6 @@ export default {
     height: {
       type: [Number, String],
       default: 24
-    }
-  },
-  computed: {
-    svg() {
-      return feather.icons[this.name || 'icon'].toSvg({
-        class: 'icon',
-        width: this.width,
-        height: this.height
-      })
     }
   }
 }
