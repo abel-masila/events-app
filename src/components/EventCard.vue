8c899787bc2831605b70ec67155bf3b8c7edbc0b
Abel Masila <abelmasila@gmail.com>
2020-07-23 13:14:38 +0300

    vue-router

---
+++ src/components/EventCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1></h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'test out'
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,34 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/about-us',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/about',
+    redirect: { name: 'About' }
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
