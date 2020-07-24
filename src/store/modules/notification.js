23938ff1799bed6a3c4f520c556984340aebb13b
Abel Masila <abelmasila@gmail.com>
2020-07-24 16:38:11 +0300

    added notification module

---
+++ src/store/modules/notification.js
@@ -0,0 +1,26 @@
+export const namespaced = true
+export const state = {
+  notifications: []
+}
+let nextId = 1
+export const mutation = {
+  PUSH(state, notification) {
+    state.notifications.push({
+      ...notification,
+      id: nextId++
+    })
+  },
+  DELETE(state, notificationToRemove) {
+    state.notifications = state.notifications.filter(
+      item => item.id !== notificationToRemove.id
+    )
+  }
+}
+export const actions = {
+  add({ commit }, notification) {
+    commit('PUSH', notification)
+  },
+  remove({ commit }, notificationToRemove) {
+    commit('DELETE', notificationToRemove)
+  }
+}
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -28,15 +28,16 @@
   </div>
 </template>
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 export default {
   props: ['id'],
   created() {
-    this.$store.dispatch('event/fetchEvent', this.id)
+    this.fetchEvent(this.id)
   },
   computed: {
     ...mapState({ event: state => state.event.event })
-  }
+  },
+  methods: mapActions('event', ['fetchEvent'])
 }
 </script>
 <style scoped>
