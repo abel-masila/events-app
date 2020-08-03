0852584d276f6870822247d239ea259bfe4bed53
Abel Masila <abelmasila@gmail.com>
2020-08-03 16:40:24 +0300

    added BaseInput component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="field">
+    <label v-if="label">{{ label }}</label>
+    <input type="text" @input="updateValue" :value="value" v-bind="$attrs" />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: [String, Number]
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -7,31 +7,18 @@
         <option v-for="cat in categories" :key="cat">{{ cat }}</option>
       </select>
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input
-          v-model="event.title"
-          type="text"
-          placeholder="Add an event title"
-        />
-      </div>
-      <div class="field">
-        <label>Description</label>
-        <input
-          v-model="event.description"
-          type="text"
-          placeholder="Add a description"
-        />
-      </div>
+      <BaseInput label="Title" v-model="event.title" placeholder="Title" />
+      <BaseInput
+        label="Description"
+        v-model="event.description"
+        placeholder="Add a description"
+      />
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input
-          v-model="event.location"
-          type="text"
-          placeholder="Add a location"
-        />
-      </div>
+      <BaseInput
+        label="Location"
+        v-model="event.location"
+        placeholder="Add a location"
+      />
       <h3>When is your event?</h3>
       <div class="field">
         <label>Date</label>
